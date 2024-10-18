e15a2f429f86f8ffcefd5785efce2c30a6c2b1ad
heiwa4126 <heiwa4126@gmail.com>
2024-10-18 17:22:37 +0900

    Walkthrough 3

--- src/Walkthroughs.tsx
+++ src/Walkthroughs.tsx
@@ -1,12 +1,14 @@
 // https://docs.slatejs.org/walkthroughs/01-installing-slate
+// https://docs.slatejs.org/walkthroughs/02-adding-event-handlers
+// https://docs.slatejs.org/walkthroughs/03-defining-custom-elements
 // Import React dependencies.
-import { useState } from "react";
+import { useCallback, useState } from "react";
 // Import the Slate editor factory.
-import { type BaseEditor, createEditor } from "slate";
+import { type BaseEditor, Editor, Element, Transforms, createEditor } from "slate";
 // Import the Slate components and React plugin.
 import { Editable, type ReactEditor, Slate, withReact } from "slate-react";
 
-type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomElement = { type: "paragraph" | "code"; children: CustomText[] };
 type CustomText = { text: string };
 
 declare module "slate" {
@@ -24,20 +26,56 @@ const initialValue: CustomElement[] = [
 	},
 ];
 
+// Define a React component renderer for our code blocks.
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const CodeElement = (props: any) => {
+	return (
+		<pre {...props.attributes}>
+			<code>{props.children}</code>
+		</pre>
+	);
+};
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const DefaultElement = (props: any) => {
+	return <p {...props.attributes}>{props.children}</p>;
+};
+
 // Define our app...
 const App = () => {
 	// Create a Slate editor object that won't change across renders.
 	const [editor] = useState(() => withReact(createEditor()));
+
+	// Define a rendering function based on the element passed to `props`. We use
+	// `useCallback` here to memoize the function for subsequent renders.
+	const renderElement = useCallback((props: { element: { type: string } }) => {
+		switch (props.element.type) {
+			case "code":
+				return <CodeElement {...props} />;
+			default:
+				return <DefaultElement {...props} />;
+		}
+	}, []);
+
 	// Add the editable component inside the context.
 	return (
 		<Slate editor={editor} initialValue={initialValue}>
 			<Editable
+				renderElement={renderElement}
 				onKeyDown={(event) => {
-					if (event.key === "&") {
-						// Prevent the ampersand character from being inserted.
+					if (event.key === "'" && event.ctrlKey) {
+						// ctrl+`は使ってるので、ctrl+'に変更
+						console.log("ctrl+' was pressed");
 						event.preventDefault();
-						// Execute the `insertText` method when the event occurs.
-						editor.insertText("and");
+						// Determine whether any of the currently selected blocks are code blocks.
+						const [match] = Editor.nodes(editor, {
+							match: (n) => (n as Element).type === "code",
+						});
+						// Toggle the block type depending on whether there's already a match.
+						Transforms.setNodes(
+							editor,
+							{ type: match ? "paragraph" : "code" },
+							{ match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) },
+						);
 					}
 				}}
 			/>
