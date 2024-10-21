941d80df9f48ed1bce2066beee8b9624fdc8e848
heiwa4126 <heiwa4126@gmail.com>
2024-10-21 13:39:51 +0900

    Walkthrough 4

--- src/Walkthroughs.tsx
+++ src/Walkthroughs.tsx
@@ -1,12 +1,20 @@
 // https://docs.slatejs.org/walkthroughs/01-installing-slate
 // https://docs.slatejs.org/walkthroughs/02-adding-event-handlers
 // https://docs.slatejs.org/walkthroughs/03-defining-custom-elements
+// https://docs.slatejs.org/walkthroughs/04-applying-custom-formatting
 // Import React dependencies.
 import { useCallback, useState } from "react";
 // Import the Slate editor factory.
 import { type BaseEditor, Editor, Element, Transforms, createEditor } from "slate";
 // Import the Slate components and React plugin.
-import { Editable, type ReactEditor, type RenderElementProps, Slate, withReact } from "slate-react";
+import {
+	Editable,
+	type ReactEditor,
+	type RenderElementProps,
+	RenderLeafProps,
+	Slate,
+	withReact,
+} from "slate-react";
 
 type CustomElement = { type: "paragraph" | "code"; children: CustomText[] };
 type CustomText = { text: string };
@@ -38,6 +46,23 @@ const DefaultElement = (props: RenderElementProps) => {
 	return <p {...props.attributes}>{props.children}</p>;
 };
 
+// Define a React component to render leaves with bold text.
+// カスタムのLeaf型を定義
+interface CustomLeaf {
+	bold?: boolean;
+	// 他のカスタム属性があればここに追加
+}
+const Leaf = (props: RenderLeafProps) => {
+	return (
+		<span
+			{...props.attributes}
+			style={{ fontWeight: (props.leaf as CustomLeaf).bold ? "bold" : "normal" }}
+		>
+			{props.children}
+		</span>
+	);
+};
+
 // Define our app...
 const App = () => {
 	// Create a Slate editor object that won't change across renders.
@@ -54,26 +79,44 @@ const App = () => {
 		}
 	}, []);
 
+	// Define a leaf rendering function that is memoized with `useCallback`.
+	const renderLeaf = useCallback((props: RenderLeafProps) => {
+		return <Leaf {...props} />;
+	}, []);
+
 	// Add the editable component inside the context.
 	return (
 		<Slate editor={editor} initialValue={initialValue}>
 			<Editable
 				renderElement={renderElement}
+				renderLeaf={renderLeaf}
 				onKeyDown={(event) => {
-					if (event.key === "'" && event.ctrlKey) {
-						// ctrl+`は使ってるので、ctrl+'に変更
-						console.log("ctrl+' was pressed");
-						event.preventDefault();
-						// Determine whether any of the currently selected blocks are code blocks.
-						const [match] = Editor.nodes(editor, {
-							match: (n) => (n as Element).type === "code",
-						});
-						// Toggle the block type depending on whether there's already a match.
-						Transforms.setNodes(
-							editor,
-							{ type: match ? "paragraph" : "code" },
-							{ match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) },
-						);
+					if (!event.ctrlKey) {
+						return;
+					}
+					switch (event.key) {
+						case "'": {
+							// ctrl+`は使ってるので、ctrl+'に変更
+							console.log("ctrl+' was pressed");
+							event.preventDefault();
+							// Determine whether any of the currently selected blocks are code blocks.
+							const [match] = Editor.nodes(editor, {
+								match: (n) => (n as Element).type === "code",
+							});
+							// Toggle the block type depending on whether there's already a match.
+							Transforms.setNodes(
+								editor,
+								{ type: match ? "paragraph" : "code" },
+								{ match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) },
+							);
+							break;
+						}
+						// When "B" is pressed, bold the text in the selection.
+						case "b": {
+							event.preventDefault();
+							Editor.addMark(editor, "bold", true);
+							break;
+						}
 					}
 				}}
 			/>
